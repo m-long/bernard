22b4b127cef8bab488c255d71085bb45d080ba95
m-long <m@mattlong.la>
2017-06-21 18:45:03 -0700

    Add flags validator

--- app/models/remote.rb
+++ app/models/remote.rb
@@ -21,4 +21,36 @@ class Remote < ApplicationRecord
   validates :duty_cycle,
               allow_nil: true,
               inclusion: { in: 0..100 }
+  ## custom validator for flags
+  validate :flags_are_formatted_properly
+
+  # Private methods
+
+  private
+
+    def flags_are_formatted_properly
+      allowed_flags = %w( RC5
+                          RC6
+                          RCMM
+                          SHIFT_ENC
+                          SPACE_ENC
+                          REVERSE
+                          NO_HEAD_REP
+                          NO_FOOT_REP
+                          CONST_LENGTH
+                          RAW_CODES
+                          REPEAT_HEADER
+      )
+      flags = flags.split("|").strip
+      # make sure no repeated flags
+      if flags.unique != flags
+        errors.add(:flags, "must not have repeated flags")
+      end
+      # validate all flags are allowed, must be case-sensitive
+      flags.each do |flag|
+        unless flag.in? allowed_flags
+          errors.add(:flags, "has improper flag: #{flag}")
+        end
+      end
+    end
 end
