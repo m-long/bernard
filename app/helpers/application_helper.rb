6f3cc12831c17b06e5d09847969375b9fc3918ce
m-long <m@mattlong.la>
2017-06-27 10:17:01 -0700

    Make user helper methods globally available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,25 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis
   def full_title(page_title="")
-    base_title="mattlong.la"
+    base_title="Bernard.ai"
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
+  end
+
+  # Confirms a logged in user
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in to access this page."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms an admin user
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,7 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   def setup
-    @base_title="mattlong.la"
+    @base_title="Bernard.ai"
   end
 
   # Returns true if a test user is logged in
