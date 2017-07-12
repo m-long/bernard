26cd2d38cb6a46934d86970c98e6bb788f268678
m-long <m@mattlong.la>
2017-07-12 11:53:05 -0700

    Implement mqtt messenging

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -9,134 +9,58 @@ class SkillsController < ApplicationController
     input = AlexaRubykit::build_request(params)
     output = AlexaRubykit::Response.new
     session_end = true
-    message = "There was an error"
+    message = "There was an error."
+    voice_response = "I'm sorry, there was an error."
 
-    alexa_skill_intents = AlexaSkillIntent.all
+    # Initialize variables
+    location = ""
+    device = ""
+    mqtt_topic = "uid"
 
     case input.type.upcase
     when "LAUNCH_REQUEST"
       # User talked to Bernard but did not say something matching an intent"
-      message = "This is Bernard. How can I help you?"
+      voice_response = "This is Bernard. How can I help you?"
     when "SESSION_ENDED_REQUEST"
       # it's over
-      message = nil
+      message = ""
+      voice_response = ""
     when "INTENT_REQUEST"
       input_name  = input.name.downcase
       input_slots = input.slots
       # not memory or speed optimized but works for now
+      alexa_skill_intents = AlexaSkillIntent.all
       alexa_skill_intents.each do |intent|
         if intent.name.downcase == input_name
-          message = intent.voice_response
+          message = intent.message
+          voice_response = intent.voice_response
           intent.slots.each do |slot|
-            if input_slots[slot]
-              slot_value = input_slots[slot]["value"]
+            if input_slots[slot.name]
+              slot_value = input_slots[slot.name]["value"]
               message << " #{slot_value}"
+              logger.info "Slot detected, message is #{message}"
             end
           end
+          # Craft MQTT topic | make this a separate function later?
+          device = input_slots["Device"]["value"].downcase if input_slots["Device"]
+          room   = input_slots["Room"]["value"].downcase if input_slots["Room"]
+          mqtt_topic << "/#{room}" if !room.nil?
+          mqtt_topic << "/#{device}" if !device.nil?
+
+          # Send MQTT Command
+          send_mqtt_command mqtt_topic, message
+          logger.info "MQTT Topic: #{mqtt_topic}"
+          break # stop iterating through intents
         end
       end
     end
 
     c_type = "Simple"
     c_title = "Bernard"
-    c_subtitle = "TV Remote"
+    c_subtitle = "Bedroom TV"
     # output.add_card(c_type, c_title, c_subtitle, message) unless message.blank?
 
-    output.add_speech(message) unless message.blank?
+    output.add_speech(voice_response) unless voice_response.blank?
     render json: output.build_response(session_end)
   end
-
-  def old_interface
-    input  = AlexaRubykit::build_request(params)
-    output = AlexaRubykit::Response.new
-    session_end = true
-    message = "There was an error"
-
-    case input.type
-    when "LAUNCH_REQUEST"
-      # user talked to our skill but did not say something matching intent
-      message = "This is Bernard. How can I help you?"
-    when "INTENT_REQUEST"
-      case input.name
-      when "PowerCycle"
-        # our custom, simple intent from above that user matched
-        message = "Tv power."
-        send_command(message)
-      when "VolumeChange"
-        direction = input.slots["Direction"]["value"]
-        message = "Turn volume #{direction}"
-
-        if input.slots["Count"]
-          count = input.slots["Count"]["value"] || 1
-        else
-          count = 1
-        end
-        if count.to_i && count.to_i > 1
-          message << " by #{count}"
-        end
-
-        send_command(message)
-      when "VolumeMute"
-        message = "Tv muted."
-        command = "volume mute"
-        send_command(command)
-      when "ChannelChange"
-        direction = input.slots["Direction"]["value"]
-        message = "Turn channel #{direction}"
-
-        count = input.slots["Count"]["value"] || 1
-        if count.to_i && count.to_i > 1
-          message << " by #{count}"
-        end
-
-        send_command(message)
-      when "ChannelSet"
-        channel_number = input.slots["ChannelNumber"]["value"]
-        if channel_number && channel_number.to_i >=0
-          message = "Change channel to #{channel_number}"
-          send_command(message)
-        else
-          message = "Sorry, I did not understand that channel number."
-        end
-      when "ChannelPrevious"
-        message = "Previous channel"
-
-        send_command(message)
-      when "SourceChange"
-        message = "Change source"
-        if input.slots && input.slots["TVSource"]
-          tv_source = input.slots["TVSource"]["value"]
-          if tv_source && [ "cable", "tv"].include?(tv_source.downcase)
-            message << " to #{tv_source}"
-          end
-        end
-        send_command(message)
-      end
-    when "SESSION_ENDED_REQUEST"
-      # it's over
-      message = nil
-    end
-
-    c_type = "Simple"
-    c_title = "Bernard"
-    c_subtitle = "TV Remote"
-    # output.add_card(c_type, c_title, c_subtitle, message) unless message.blank?
-
-    output.add_speech(message) unless message.blank?
-    render json: output.build_response(session_end)
-  end
-
-  private
-
-    def send_command(tv_command)
-      MQTT::Client.connect(
-        host: 'bernard.mattlong.la',
-        port: 8883,
-        username: 'matt',
-        password: 'fuckthis',
-        ssl: true
-      ) do |c|
-        c.publish("Samsung", "#{tv_command.downcase}")
-      end
-    end
 end
--- app/helpers/mqtt_helper.rb
+++ app/helpers/mqtt_helper.rb
@@ -1,13 +1,27 @@
 module MqttHelper
-  client          = MQTT::Client.new
-  client.host     = 'mqtt.mattlong.la'
-  client.port     = '8883'
-  client.username = Rails.application.secrets.mqtt_username
-  client.password = Rails.application.secrets.mqtt_password
-  client.ssl      = true
 
-  def send_mqtt_command topic, message
-    client.connect() do
+  def send_mqtt_command topic, message, options: { 
+    client: MQTT::Client.new, 
+    host: Rails.application.secrets.mqtt_host,
+    port: '8883', 
+    username: Rails.application.secrets.mqtt_username, 
+    password: Rails.application.secrets.mqtt_password, 
+    ssl: true }
+
+    # Define connection
+    client          = options[:client]
+    client.host     = options[:host]
+    client.port     = options[:port]
+    client.username = options[:username] 
+    client.password = options[:password] 
+    client.ssl      = options[:ssl] 
+
+    logger.info "Client: #{client}"
+    logger.info "MQTT Host: #{client.host}"
+    logger.info "MQTT Username: #{client.username}"
+    logger.info "MQTT SSL: #{client.ssl}"
+
+    client.connect() do |c|
       c.publish(topic, message)
     end
   end
